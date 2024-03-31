3f0a4455bd5731c32d7319c1dd7a3f809c5a297a
Desai Dhruvi <desaidhruvi970@gmail.com>
2024-03-31 12:19:36 +0530

    main

--- 01NewsWeb/src/Components/LoginForm.jsx
+++ 01NewsWeb/src/Components/LoginForm.jsx
@@ -29,31 +29,31 @@ function LoginForm({ onClose }) {
 
   return (
     <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center z-50 bg-gray-900 bg-opacity-50" onClick={handleClickOutside}>
-      <div className="bg-gradient-to-br from-gray-800 to-gray-700 p-8 rounded-md shadow-md transform scale-105">
-        <h2 className="text-3xl font-bold mb-6 text-center text-white">Login/Register</h2>
+      <div className="bg-gray-100 p-8 rounded-lg shadow-xl">
+        <h2 className="text-3xl font-bold mb-6 text-center text-gray-800">Login/Register</h2>
         <input
           type="email"
           placeholder="Email"
-          className="w-full mb-4 px-4 py-3 bg-gray-800 border border-gray-600 rounded-md focus:outline-none focus:border-blue-500 text-white"
+          className="w-full mb-4 px-4 py-3 bg-gray-200 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500 text-gray-800"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
         />
         <input
           type="password"
           placeholder="Password"
-          className="w-full mb-4 px-4 py-3 bg-gray-800 border border-gray-600 rounded-md focus:outline-none focus:border-blue-500 text-white"
+          className="w-full mb-4 px-4 py-3 bg-gray-200 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500 text-gray-800"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
         />
         <div className="flex justify-between">
-          <button onClick={handleLogin} className="w-1/2 px-6 py-3 bg-red-800 text-white rounded-md mr-2 hover:bg-black focus:outline-none focus:bg-blue-600">
+          <button onClick={handleLogin} className="w-1/2 px-6 py-3 bg-blue-500 text-white rounded-md mr-2 hover:bg-blue-600 focus:outline-none focus:bg-blue-600">
             Login
           </button>
-          <button onClick={handleRegister} className="w-1/2 px-6 py-3 bg-green-500 text-white rounded-md hover:bg-green-600 focus:outline-none focus:bg-green-600">
+          <button onClick={handleRegister} className="w-1/2 px-6 py-3 bg-gray-600 text-white rounded-md hover:bg-gray-700 focus:outline-none focus:bg-gray-700">
             Register
           </button>
         </div>
-        <button onClick={onClose} className="mt-6 w-full px-6 py-3 bg-gray-600 text-white rounded-md hover:bg-gray-700 focus:outline-none focus:bg-gray-700">
+        <button onClick={onClose} className="mt-6 w-full px-6 py-3 bg-gray-300 text-gray-800 rounded-md hover:bg-gray-400 focus:outline-none focus:bg-gray-400">
           Close
         </button>
         {showPopup && (
